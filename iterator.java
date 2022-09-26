6f8acd14710c8e7494c9b15d99faa21cd162f659
ericon67 <63900489+ericon67@users.noreply.github.com>
2022-09-26 18:12:35 +0530

    Create iterator.java

---
+++ iterator.java
@@ -0,0 +1,42 @@
+//three applications are there. 1 can call next element 2. can iterate using a while loop unlit last 3. can use to remove elements from a datatype
+public class Main {
+  public static void main(String[] args) {
+
+    // Make a collection
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+
+    // Get the iterator
+    Iterator<String> it = cars.iterator();
+
+    // Print the first item
+    System.out.println(it.next());
+  }
+}
+while(it.hasNext()) {
+  System.out.println(it.next());
+}
+
+
+
+///////////////////////
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    numbers.add(12);
+    numbers.add(8);
+    numbers.add(2);
+    numbers.add(23);
+    Iterator<Integer> it = numbers.iterator();
+    while(it.hasNext()) {
+      Integer i = it.next();
+      if(i < 10) {
+        it.remove();
+      }
+    }
+    System.out.println(numbers);
+  }
+}
